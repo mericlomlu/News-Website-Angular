a38d060ce5261860139c84e933e0b6f50af967ff
Meriç <mericlomlu@gmail.com>
2022-11-03 16:31:00 +0300

    Creating Pages & Templates
    Created all the necessary page components for news categories and created a news template for showing the news.

--- news-website/src/app/core/core.module.ts
+++ news-website/src/app/core/core.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './components/main/main.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ScienceComponent } from './pages/science/science.component';
+import { SportsComponent } from './pages/sports/sports.component';
+import { BusinessComponent } from './pages/business/business.component';
+import { GeneralComponent } from './pages/general/general.component';
+import { HealthComponent } from './pages/health/health.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
+import { EntertainmentComponent } from './pages/entertainment/entertainment.component';
+import { NewsTemplateComponent } from './templates/news-template/news-template.component';
 
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    ScienceComponent,
+    SportsComponent,
+    BusinessComponent,
+    GeneralComponent,
+    HealthComponent,
+    TechnologyComponent,
+    EntertainmentComponent,
+    NewsTemplateComponent
   ],
   imports: [
     CommonModule
---
+++ news-website/src/app/core/templates/news-template/news-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-template',
+  templateUrl: './news-template.component.html',
+  styleUrls: ['./news-template.component.scss']
+})
+export class NewsTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
